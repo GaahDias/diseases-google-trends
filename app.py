265c16c87c24e50827e7af7ef3ee5634d599bcfa
Dias <Gabriel.Dias@sanofi.com>
2022-03-31 09:49:45 -0300

    Change from User Input to CL arguments, and add one more column 'region_id' to csv

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 # Google trends script
-# The purpose of this script is to get data from Google Trends, and compile that into a csv file
+# The purpose of this script is to get data from Google Trends about diseases
+# from a specific country, and it's subregions. 
 # Written by Gabriel Dias
 
 # Imports
+import sys
 import os
 from pathlib import Path
 from time import strftime
@@ -20,67 +22,48 @@ from dateutil.relativedelta import relativedelta
 # inc_sub_region - bool flag to include or not subregions in search.
 # today_date - just a var to hold today's date, which will be used later on Google Trends search.
 # year_month - year and month from today_date's.
+# year - in how many years the search is going to be made
 # search_type - the type of search that will be done in Google Trends.
 pytrends = TrendReq(hl='en-US', tz=360)
 keywords = []
 suggestions = []
 geo_code = ''
 geo_codes = []
-inc_sub_reg = False
 today_date = date.today()
 year_month = today_date.strftime('%Y-%m')
+year = 5
 search_type = 'Disease'
 
-# Welcome msg
-print("============================\n" +
-      "|   GOOGLE TRENDS SCRIPT   |\n" +
-      "============================\n")
+geo_code = sys.argv[1]
+geo_codes.append(geo_code)
+keywords = sys.argv[2:]
 
-# Getting user input
-while True:
-    tmp_kw = input("What keywords you wish to search? (Eg.: Allergy, Influenza, ...)\nIf you are done, just press enter.\n")
-    if tmp_kw == "":
-        geo_code = input("What geo code you wish to search? (Eg.: MX, US, JP, ...)\n")
-        geo_codes.append(geo_code)
-
-        while True:
-            tmp_sub_reg = input("\nDo you wish to search in all subregions? (y/n).\n")
-            if tmp_sub_reg.lower() == 'y':
-                inc_sub_reg = True
-                break
-            elif tmp_sub_reg.lower() == 'n':
-                inc_sub_reg = False
-                break
-            else:
-                print("\nPlease, insert y or n.")
-        break
-    keywords.append(tmp_kw)
 
 # Getting suggestion code based on keyword
 for x in range(len(keywords)):
     tmp_suggestion = pytrends.suggestions(keywords[x])
-    for x in range(len(tmp_suggestion)):
-        if tmp_suggestion[x]['type'] == search_type:
-            tmp_suggestion = tmp_suggestion[x]
+    for y in range(len(tmp_suggestion)):
+        if tmp_suggestion[y]['type'] == search_type:
+            tmp_suggestion = tmp_suggestion[y]
             break
     suggestions.append(tmp_suggestion)
 
-# Getting all subregions
-if inc_sub_reg == True:
-    # Simple 3 months search just to get the subregions
-    for x in range(len(suggestions)):
-        pytrends.build_payload([suggestions[x]['mid']],
-        cat=0,
-        timeframe=str((today_date - relativedelta(months=3))) + ' ' + str(today_date),
-        geo=geo_code)
 
-        # Getting all subregions (inc_low_vol) with it's geo codes (inc_geo_code)
-        tmp_gcs = pytrends.interest_by_region(resolution='COUNTRY', inc_low_vol=True, inc_geo_code=True)
+# Getting all subregions with a simple 3 months search
+for x in range(len(suggestions)):
+    pytrends.build_payload([suggestions[x]['mid']],
+    cat=0,
+    timeframe=str((today_date - relativedelta(months=3))) + ' ' + str(today_date),
+    geo=geo_code)
 
-        # Adding all geo codes into our var geo_codes for later usage
-        for y in range(len(tmp_gcs['geoCode'])):
-            if tmp_gcs['geoCode'][y] not in geo_codes:
-                geo_codes.append(tmp_gcs['geoCode'][y])
+    # Getting all subregions (inc_low_vol) with it's geo codes (inc_geo_code)
+    tmp_gcs = pytrends.interest_by_region(resolution='COUNTRY', inc_low_vol=True, inc_geo_code=True)
+
+    # Adding all geo codes into our var geo_codes for later usage
+    for y in range(len(tmp_gcs['geoCode'])):
+        if tmp_gcs['geoCode'][y] not in geo_codes:
+            geo_codes.append(tmp_gcs['geoCode'][y])
+
 
 # Searching all suggestions (disease) and subregions on Google Trends
 for x in range(len(suggestions)):
@@ -92,7 +75,7 @@ for x in range(len(suggestions)):
         pytrends.build_payload([suggestions[x]['mid']], 
         cat=0, 
         # Collecting data from one year ago - 2022-03-29 2021-03-29 for eg.
-        timeframe=str((today_date - relativedelta(years=1))) + ' ' + str(today_date),
+        timeframe=str((today_date - relativedelta(years=year))) + ' ' + str(today_date),
         # Search by geo codes
         geo=geo_codes[y])
 
@@ -102,6 +85,7 @@ for x in range(len(suggestions)):
             # Data cleansing
             tmp_data = tmp_data.drop(labels=['isPartial'],axis='columns')
             tmp_data = tmp_data.rename(columns={suggestions[x]['mid']: suggestions[x]['title']})
+            tmp_data['region_id'] = geo_codes[y]
 
         try:
             # Creating folder to store the csvs
